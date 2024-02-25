643b267661c58ee458787e86c08fe6468079fc85
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-02-25 12:29:38 +0500

    add

--- src/lib/postgres.js
+++ src/lib/postgres.js
@@ -1,17 +1,15 @@
 const { Pool } = require("pg");
 const { connection } = require("../config");
 
-// const credentials = {
-//   user: "tamalpro_database_user",
-//   host: "localhost",
-//   database: "tamalpro_database",
-//   password: "spaceteam9959",
-//   port: 5432,
-// };
+const credentials = {
+  user: "sellcent",
+  host: "localhost",
+  database: "sellcent_click",
+  password: "eA9WS3s4(ym8F]",
+  port: 5432,
+};
 
-const pool = new Pool({
-  connectionString: connection.connectionStringEL,
-});
+const pool = new Pool(credentials);
 
 const fetch = async (SQL, ...params) => {
   const client = await pool.connect();
